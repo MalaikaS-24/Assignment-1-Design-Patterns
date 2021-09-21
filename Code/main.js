2b6f7c3eb63451da6ebc9fd6fa4bedee1e025bb4
Andrew M <32660013+mikandrew53@users.noreply.github.com>
2021-09-21 14:57:27 -0400

    Update main.js

--- Code/main.js
+++ Code/main.js
@@ -1,6 +1,6 @@
 import { Factory } from './Factory';
 
-const factory = new Factory(itemPrices);
+const factory = new Factory();
 
 const item_one = factory.createItem();
 const item_two = factory.createItem();
